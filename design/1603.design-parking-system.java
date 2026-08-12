fc42d34200357ea55e3624b5c42f3c6b1bd688ce
yosief14 <yosief10@outlook.com>
2026-08-12 17:04:20 -0600

    parking system design problem

---
+++ design/1603.design-parking-system.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1603 lang=java
+ *
+ * [1603] Design Parking System
+ */
+
+// @lc code=start
+class ParkingSystem {
+
+    public ParkingSystem(int big, int medium, int small) {
+       
+    }
+    
+    public boolean addCar(int carType) {
+        
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
+// @lc code=end
+
