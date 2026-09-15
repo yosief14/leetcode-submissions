65cb8a5f7844be431e44ee9f7706d9143dcfa353
yosief14 <yosief10@outlook.com>
2026-09-15 16:45:50 -0600

    formating copy list with random pointer solution

--- linked-lists/138.copy-list-with-random-pointer.java
+++ linked-lists/138.copy-list-with-random-pointer.java
@@ -22,8 +22,28 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        
+        Node copy = new Node(0);
+        Node start = copy;
+        Node headStar = head;
+        HashMap<Node, Node> list = new HashMap<>();
+        while (head != null) {
+            copy.next = new Node(head.val);
+            copy = copy.next;
+            list.put(head, copy);
+            copy.random = head.random;
+            head = head.next;
+        }
+        copy = start.next;
+
+        while (copy != null) {
+            copy.random = list.get(copy.random);
+            copy = copy.next;
+        }
+        // list.forEach((k, v) -> System.out.println(" key: " + (k != null ?k.val : k) +
+        // " val:"+v.val));
+
+        return start.next;
+
     }
 }
 // @lc code=end
-
