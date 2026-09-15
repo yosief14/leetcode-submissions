75a21068f7955fbc3830d6534f9d3e0e09db374d
yosief14 <yosief10@outlook.com>
2026-09-15 16:47:11 -0600

    Reorder linked List solution

---
+++ linked-lists/143.reorder-list.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=143 lang=java
+ *
+ * [143] Reorder List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+
+        // First step finding mid point using slow and fast pointer
+
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Second step is to reverse
+        ListNode second = slow.next;
+
+        // break list at half way point
+        slow.next = null;
+        ListNode prev = null;
+
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+
+        // Third step is to merge
+
+        ListNode first = head;
+        second = prev;
+
+        System.out.println(second.val + " " + second.next.val + " " + second.next.next);
+        System.out.println(first.val + " " + second.next.val + " " + second.next.next);
+        while (second != null) {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+            first.next = second;
+            second.next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+
+    }
+}
+// @lc code=end
